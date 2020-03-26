cc62fc8f7f573749f8195a62c1d4eef111ea2635
Bryan Silva <brandonb2816@gmail.com>
2020-03-26 20:59:50 -0300

    Configurando pug y creando las vistas

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 // Importar Express y Rutas
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 // Configurar Express
 const app = express();
+
+// Habilitar Pug
+app.set('view engine', 'pug');
+
+// Añadir las vistas
+app.set('views', path.join(__dirname, './views'));
 
 // Cargar las rutas
 app.use('/', routes());
