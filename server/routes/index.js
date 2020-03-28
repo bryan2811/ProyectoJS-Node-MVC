a810ff2a0d7b63e99775c5a7f09f7103a7f9a6fb
Bryan Silva <brandonb2816@gmail.com>
2020-03-28 13:43:30 -0300

    Añadiendo los controladores al MVC

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,99 +1,19 @@
 const express = require('express')
 const router = express.Router();
 
-const Viaje = require('../models/Viajes');
-const Testimonial = require('../models/Testimoniales');
+// Importando Controladores
+const homeController = require('../controllers/homeController');
+const nosotrosController = require('../controllers/nosotrosController');
+const viajesController = require('../controllers/viajesController');
+const testimonialesController = require('../controllers/testimonialesController');
 
 module.exports = function() {
-    router.get('/', (req, res) => {
-        const promises = [];
-        
-        promises.push(Viaje.findAll({
-            limit: 3
-        }))
-
-        promises.push(Testimonial.findAll({
-            limit: 3
-        }))
-
-        // Pasar el promise y ejecutarlo
-        const resultado = Promise.all(promises);
-        
-        resultado.then(resultado => res.render('index', {
-            pagina: 'Próximos Viajes',
-            clase: 'home',
-            viajes : resultado[0],
-            testimoniales : resultado[1]
-        }))
-        .catch(error => console.log(error))
-    }); // Página de Inicio
-
-    router.get('/nosotros', (req, res) => {
-        res.render('nosotros', {
-            pagina: 'Sobre Nosotros'
-        });
-    }); // Nosotros
-
-    router.get('/viajes', (req, res) => {
-        Viaje.findAll()
-            .then(viajes => res.render('viajes', {
-                pagina: 'Próximos Viajes',
-                viajes
-            }))
-            .catch(error => console.log(error))
-    }); // Viajes
-
-    router.get('/viajes/:id', (req, res) => {
-        Viaje.findByPk(req.params.id)
-            .then(viaje => res.render('viaje', {
-                viaje
-            }))
-            .catch(error => console.log(error))
-    }); // Viaje c/u
-
-    router.get('/testimoniales', (req, res) => {
-        Testimonial.findAll()
-            .then(testimoniales => res.render('testimoniales', {
-                pagina: 'Testimoniales',
-                testimoniales
-            }))
-    }); // Testimoniales GET
-
-    router.post('/testimoniales', (req, res) => {
-        // Validar que todos los campos estén llenos
-        let {nombre, correo, mensaje} = req.body;
-
-        let errores = [];
-        if (!nombre) {
-            errores.push({'mensaje' : 'Agrega tu nombre'})
-        }
-        if (!correo) {
-            errores.push({'mensaje' : 'Agrega tu correo'})
-        }
-        if (!mensaje) {
-            errores.push({'mensaje' : 'Agrega tu mensaje'})
-        }
-
-        // Revisar por errores
-        if (errores.length > 0) {
-            // Muestra la vista con errores
-            res.render('testimoniales', {
-                errores,
-                nombre,
-                correo,
-                mensaje
-            })
-        } else {
-            // Almacenarlo en la BD
-            Testimonial.create({
-                nombre,
-                correo,
-                mensaje
-            })
-            .then(testimonial => res.redirect('/testimoniales'))
-            .catch(error => console.log(error))
-        }
-    }); // Testimoniales POST
-
+    router.get('/', homeController.consultasHomepage); // Página de Inicio
+    router.get('/nosotros', nosotrosController.infoNosotros); // Página de Nosotros
+    router.get('/viajes', viajesController.mostrarViajes); // Página de Viajes
+    router.get('/viajes/:id', viajesController.mostrarViaje); // Página de Viaje c/u
+    router.get('/testimoniales', testimonialesController.mostrarTestimoniales); // Testimoniales GET
+    router.post('/testimoniales', testimonialesController.agregarTestimonial); // Testimoniales POST
+    
     return router;
 }
